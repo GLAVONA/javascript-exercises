ff91976c57c38a44527883f5fae3cf1218a009c3
GLAVONA <97589269+GLAVONA@users.noreply.github.com>
2022-11-14 17:50:37 +0200

    solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = (people)=>{    
+    const sort = people.sort((lastOne, nextOne)=>{
+        if (!lastOne.hasOwnProperty('yearOfDeath')){
+            lastOne.yearOfDeath = 2022;
+        }
+        if (!nextOne.hasOwnProperty('yearOfDeath')){
+            nextOne.yearOfDeath = 2022;
+        }
+        
+        const lastYear = lastOne.yearOfDeath - lastOne.yearOfBirth;
+        const nextYear = nextOne.yearOfDeath - nextOne.yearOfBirth;
+       
+        return lastYear>nextYear?-1:1;
+    })
+    return people[0];
 };
 
 // Do not edit below this line
