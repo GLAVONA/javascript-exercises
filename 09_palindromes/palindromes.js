be5e19d4c8c301a32b63fbb4ccf3e639b11e6657
GLAVONA <97589269+GLAVONA@users.noreply.github.com>
2022-11-14 16:05:25 +0200

    completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
-};
+const palindromes = function (str) {
+    const strip = str.replace(/[^a-zA-Z]/g,"").toLowerCase();
+    const revStrip = strip.split('').reverse().join('');
+    return revStrip===strip;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
