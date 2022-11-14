36fdd6e9885aa19fa3404cc0618b4b2b3ecf323f
GLAVONA <97589269+GLAVONA@users.noreply.github.com>
2022-11-14 17:08:54 +0200

    completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
-
-};
+const fibonacci = num => {
+    if (num<0){
+        return "OOPS";
+    }
+    const arr = [0,1];
+    
+    const fib = n => {
+        if (n <= 1) {
+            arr.push(n);
+          return n;
+        }
+         
+        let a = 0, b = 1, c = 1, count = 0;
+        
+        while (count < n) {
+          c = a + b;
+          a = b;
+          b = c;
+          count++;
+          arr.push(c);
+        }
+      };
+    
+      fib(num);
+    
+      return arr[num];
+    }
 
 // Do not edit below this line
 module.exports = fibonacci;
